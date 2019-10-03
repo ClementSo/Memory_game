3ec70a38a764fa6fe1160aaeaaa52702256b84cd
Clement <csortino@edenschool.com>
2019-10-03 17:05:33 +0200

    js stable

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-
-
 var td = document.getElementsByClassName("card");
+var iIndiv = document.getElementsByTagName("I");
 
 function randomArray(array) {
-  let currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue, randomIndex;
 
   while (currentIndex !== 0) {
 
@@ -18,7 +18,7 @@ function randomArray(array) {
   return array;
 }
 
-var ic = ["glass-cheers","skull-crossbones","money-bill-alt","lemon","cookie","bomb","shield-alt","rocket","glass-cheers","skull-crossbones","money-bill-alt","lemon","cookie","bomb","shield-alt","rocket"];
+var ic = ["glass-cheers", "skull-crossbones", "money-bill-alt", "lemon", "cookie", "bomb", "shield-alt", "rocket", "glass-cheers", "skull-crossbones", "money-bill-alt", "lemon", "cookie", "bomb", "shield-alt", "rocket"];
 
 ic = randomArray(ic);
 
@@ -30,12 +30,17 @@ var etat3;
 var cards = [];
 var element1;
 var element2;
+var indiv1;
+var indiv2;
 
 class Card {
   constructor(element) {
     this.frontface = "question";
     this.backface = ic[i];
-    element.addEventListener("click", () => {tdclick(this, element)});
+    this.iIndiv = iIndiv[i]
+    element.addEventListener("click", () => {
+      tdclick(this, element);
+    });
     this.element = element;
   }
 }
@@ -47,47 +52,56 @@ for (var i = 0; i < td.length; i++) {
 
 
 function tdclick(letd, element) {
-  console.log(letd, element);
-  if (!(letd == etat3)) {
-    etat3 = letd;
-    if (etat1 == false) {
-      etat1 = true;
-      card1 = letd.backface;
-      element1 = element;
-      element.style.backgroundColor = "red";
-      element.className = "fas fa-"+card1;
-      istrue(letd, element);
-      return ;
-    }
+  if (element.style.backgroundColor) {
+    if (element.style.backgroundColor == "rgba(255, 0, 207, 0)") {
+      if (etat1 == false) {
+        etat1 = true;
+        card1 = letd.backface;
+        element1 = element;
+        element.style.backgroundColor = "red";
+        letd.iIndiv.className = "fas fa-" + card1;
+        indiv1 = letd.iIndiv;
+        istrue(letd, element, indiv1, indiv2);
+        return;
+      }
     if (etat2 == false) {
       etat2 = true;
       card2 = letd.backface;
       element2 = element;
       element.style.backgroundColor = "red";
-      element.className = "fas fa-"+card2;
-      istrue(letd, element)
-      return ;
+      letd.iIndiv.className = "fas fa-" + card2;
+      indiv2 = letd.iIndiv;
+      istrue(letd, element, indiv1, indiv2);
+      return;
     }
-  }
-  console.log("une autre");
+    }
+} else {
+  element.style.backgroundColor = "rgba(255, 0, 207, 0)";
+  console.log("color");
+  tdclick(letd, element);
+}
 }
 
 
-function istrue(letd, element) {
+function istrue(letd, element, indiv1, indiv2) {
   if (etat1 == true && etat2 == true) {
-    etat1 = false;
-    etat2 = false;
     if (card1 == card2) {
       element1.style.backgroundColor = "rgba(255, 0, 207, 1)";
       element2.style.backgroundColor = "rgba(255, 0, 207, 1)";
+      etat1 = false;
+      etat2 = false;
     }
     if (!(card1 == card2)) {
-      element1.style.backgroundColor = "rgba(255, 0, 207, 0)";
-      element2.style.backgroundColor = "rgba(255, 0, 207, 0)";
-      element1.className = "fas fa-question";
-      element2.className = "fas fa-question";
+      setTimeout(function() {
+        element1.style.backgroundColor = "rgba(255, 0, 207, 0)";
+        element2.style.backgroundColor = "rgba(255, 0, 207, 0)";
+        indiv1.className = "fas fa-question";
+        indiv2.className = "fas fa-question";
+        etat1 = false;
+        etat2 = false;
+      }, 1000);
+
     }
-    tdclick(letd,element);
-    return ;
+    return;
   }
 }
